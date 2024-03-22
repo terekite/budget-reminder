2023fcbbd96457884a82a139c21ea582eb645196
Omar Ali <108383075+terekite@users.noreply.github.com>
2024-03-22 18:16:45 +0000

    Add email functionality & data pull

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
- 
-message = MIMEMultipart()
-message["To"] = 'Omar Ali'
-message["From"] = 'budget-reminder'
-message["Subject"] = 'Hello! Your Budget Reminder for the Day'
 
-title = '<b> Title line here. </b>'
-messageText = MIMEText('''Message body goes here.''','html')
-message.attach(messageText)
+# 1. Read Data
+import pandas as pd
+#https://docs.google.com/spreadsheets/d//edit?usp=sharing&ouid=114941274988909858330&rtpof=true&sd=true
+SHEET_ID = '19OXdN8kHxVJM_owhLUSOnsihDc7EOb-4'
+SHEET_NAME = 'Mar_2024'
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+df = pd.read_csv(url)
+data = df.loc[0]
+money_left = data['LEFT TO SPEND:']
 
-email = 'omarmahmoud2552@gmail.com'
-password = 'vknm omya vzje mhry'
 
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.ehlo('Gmail')
-server.starttls()
-server.login(email,password)
-fromaddr = 'omarmahmoud2552@gmail.com'
-toaddrs  = 'omar.mail@icloud.com'
-server.sendmail(fromaddr,toaddrs,message.as_string())
+# 2. Send email
+# message = MIMEMultipart()
+# message["To"] = 'Omar & Kristine'
+# message["From"] = 'Winter'
+# message["Subject"] = 'Hello! Your Budget Reminder for the Day'
+
+# title = '<b> Title line here. </b>'
+# messageText = MIMEText(f'''You have {money_left} remaining for this month.''','html')
+# message.attach(messageText)
+
+# email = 'omarmahmoud2552@gmail.com'
+# password = 'vknm omya vzje mhry'
+
+# server = smtplib.SMTP('smtp.gmail.com:587')
+# server.ehlo('Gmail')
+# server.starttls()
+# server.login(email,password)
+# fromaddr = 'omarmahmoud2552@gmail.com'
+# toaddrs  = 'omar.mail@icloud.com'
+# server.sendmail(fromaddr,toaddrs,message.as_string())
 #toaddrs  = 'kristinetangg@gmail.com'
 #server.sendmail(fromaddr,toaddrs,message.as_string())
 
-server.quit()
+# server.quit()
 
